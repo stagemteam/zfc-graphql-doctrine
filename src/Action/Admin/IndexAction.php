dfbfb2822bb79b682a6b7b6f88b83c76161c1776
Andrey <andrey.andreev1995@gmail.com>
2019-06-03 15:02:19 +0300

    reportType query has been uncommented

--- src/Action/Admin/IndexAction.php
+++ src/Action/Admin/IndexAction.php
@@ -636,7 +636,7 @@ class IndexAction extends AbstractAction
                         },
                     ],
 
-                    /*'reportType' => [
+                    'reportType' => [
                         'type' => Type::listOf($this->types->getOutput(ReportType::class)),
                         'args' => [
                             [
@@ -650,12 +650,12 @@ class IndexAction extends AbstractAction
                         ],
                         'resolve' => function ($root, $args) {
                             $qb = $this->types->createFilteredQueryBuilder(ReportType::class, $args['filter'] ?? [], $args['sorting'] ?? []);
-                            
+
                             $result = $qb->getQuery()->getResult();
 
                             return $result;
                         },
-		  ]*/
+                    ]
                 ],
                 'resolveField' => function($val, $args, $context, ResolveInfo $info) {
                     return $this->{$info->fieldName}($val, $args, $context, $info);
